7403f723a5333be5d781b9cb4f5b460daabfbbdb
shiv32 <yadav.shivkumar5@gmail.com>
2025-08-15 03:25:44 +0530

    flyweight

---
+++ Project-8/main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+#include <unordered_map>
+
+class Glyph
+{
+    char symbol;
+
+public:
+    Glyph(char c) : symbol(c) {};
+    void draw(int x, int y)
+    {
+        std::cout << "drawing " << symbol << " at (" << x << "," << y << ")" << std::endl;
+    }
+};
+
+class GlyphFactory
+{
+    std::unordered_map<char, std::shared_ptr<Glyph>> pools;
+
+public:
+    std::shared_ptr<Glyph> get(char c)
+    {
+        if (!pools[c])
+            pools[c] = std::make_shared<Glyph>(c);
+
+        return pools[c];
+    }
+
+    void poolsSize()
+    {
+        std::clog << "pools size : " << pools.size() << std::endl;
+    }
+};
+
+int main()
+{
+    GlyphFactory gf;
+
+    gf.poolsSize();
+
+    auto gfA = gf.get('A');
+    auto gfB = gf.get('B');
+
+    gfA->draw(1, 2);
+    gfB->draw(4, 6);
+
+    gf.get('A')->draw(8, 3);
+
+    gf.poolsSize();
+
+    return 0;
+}
