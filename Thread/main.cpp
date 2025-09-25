344d2ee94160f71107ea589fac0ed08aadf3845c
shiv32 <yadav.shivkumar5@gmail.com>
2025-09-26 01:51:39 +0530

    thread

--- Thread/main.cpp
+++ Thread/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <vector>
 
 // 1. function with no paramter
 void helloThread()
@@ -31,20 +32,53 @@ void incrementValue(int &value)
 
 int main(int argc, char *argv[])
 {
-    // 1. basic thread
+    // 1. basic thread ---------------------------
     std::thread t1(helloThread);
     t1.join();
+    //--------------------------------------------
 
-    // 2. thread with argument
+    // 2. thread with argument --------------------
     std::thread t2(printNumber, 1, 5);
     t2.join();
+    //---------------------------------------------
 
-    // 3. thread with ref value
+    // 3. thread with ref value -----------------------------------------
     int counter{};
     std::clog << "counter before thread start : " << counter << std::endl;
     std::thread t3(incrementValue, std::ref(counter));
     t3.join();
     std::clog << "counter after thread finish : " << counter << std::endl;
+    //--------------------------------------------------------------------
+
+    // 4. multiple thread in a loop -----------------------------------
+    std::vector<std::thread> threadContainer;
+
+    for (size_t i{}; i < 3; ++i)
+    {
+        threadContainer.emplace_back([i]()
+                                     {
+                                         std::clog << "thread #" << i << " working.." << std::endl;
+                                         std::this_thread::sleep_for(std::chrono::milliseconds(200)); });
+    }
+
+    for (auto &t : threadContainer)
+    {
+        t.join();
+    }
+    //-------------------------------------------------------------------
+
+    // 5. detach thread ---------------------------------
+    std::thread t4([]()
+                   {
+                       std::clog << "Detaching thread..." << std::endl;
+                       std::this_thread::sleep_for(std::chrono::seconds(1));
+                       std::clog << "Detach done." << std::endl; });
+    t4.detach();
+    // give time detach thread to finish
+    std::this_thread::sleep_for(std::chrono::seconds(2)); //main thread 
+    //--------------------------------------------------
+
+    std::clog << "main thread exit." << std::endl;
 
     return 0;
 }
