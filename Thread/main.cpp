fc6a9027d22b5f0f0accf001e9fe5cb1444ed41b
shiv32 <yadav.shivkumar5@gmail.com>
2025-09-26 02:26:38 +0530

    thread

--- Thread/main.cpp
+++ Thread/main.cpp
@@ -2,6 +2,7 @@
 #include <thread>
 #include <chrono>
 #include <vector>
+#include <memory>
 
 // 1. function with no paramter
 void helloThread()
@@ -29,6 +30,33 @@ void incrementValue(int &value)
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
+
+class Worker
+{
+public:
+    Worker(std::string name) : name_(std::move(name)) {}
+
+    void doWork(int iteration)
+    {
+       for(int i{}; i<iteration; ++i)
+       {
+          std::clog<<name_<<" working "<<i<<std::endl;
+          std::this_thread::sleep_for(std::chrono::milliseconds(500));
+       }
+    }
+
+private:
+    std::string name_;
+};
+
+struct Task
+{
+    void operator() (int taskNo)
+    {
+       std::clog<<"Task #"<<taskNo<<" running.."<<std::endl;
+       std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -75,8 +103,19 @@ int main(int argc, char *argv[])
                        std::clog << "Detach done." << std::endl; });
     t4.detach();
     // give time detach thread to finish
-    std::this_thread::sleep_for(std::chrono::seconds(2)); //main thread 
+    std::this_thread::sleep_for(std::chrono::seconds(2)); // main thread
     //--------------------------------------------------
+
+    //6. member function ptr -------------------------------
+    auto worker = std::make_shared<Worker>("Thread-A");
+    std::thread t5(&Worker::doWork, worker, 4);
+    t5.join();
+    //-------------------------------------------------------
+
+    //7. function object or functor ------------------------------------
+    std::thread t6(Task{}, 8);
+    t6.join();
+    //--------------------------------------------------------
 
     std::clog << "main thread exit." << std::endl;
 
